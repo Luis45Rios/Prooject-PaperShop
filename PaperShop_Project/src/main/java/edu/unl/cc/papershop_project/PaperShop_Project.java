c282f5fdd13f0fda5f8503aa54ee863e99d1020c
Luis <luis.h.rios@unl.edu.ec>
2025-11-24 14:58:28 -0500

    Se creo las clases definidas en UML

---
+++ PaperShop_Project/src/main/java/edu/unl/cc/papershop_project/PaperShop_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.unl.cc.papershop_project;
+
+/**
+ *
+ * @author luisr
+ */
+public class PaperShop_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
