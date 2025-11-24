f3b013feb3675b0f36fc8cd87c5def6d40cea876
Luis <luis.h.rios@unl.edu.ec>
2025-11-24 16:43:16 -0500

    Reubicar la clase Producto en domain

--- PaperShop_Project/src/main/java/edu/unl/cc/papershop_project/domain/Producto.java
+++ PaperShop_Project/src/main/java/edu/unl/cc/papershop_project/domain/Producto.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package edu.unl.cc.papershop_project.dominio;
+package edu.unl.cc.papershop_project.domain;
 
 /**
  *
